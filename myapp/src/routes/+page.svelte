2ea4e167249c57954e091e6ae502f386d5ee847f
lisavanmansom <lisavanmansom@gmail.com>
2024-09-17 12:11:19 +0200

    #16 html validator

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -17,7 +17,7 @@
     <ul>
         <li><a href="https://github.com/lisavanmansom">Github</a></li>
         <li><a href="https://www.linkedin.com/in/lisa-van-mansom-b78059231/">Linkedin</a></li>
-        <li><a href="mailto: lisavanmansom@gmail.com" >lisavanmansom@gmail.com</a></li>
+        <li><a href="mailto:lisavanmansom@gmail.com">Mail</a></li>
         <li><a href="tel:0642629104">0642629104</a></li>
         <li><a href="https://github.com/lisavanmansom">Learning journal</a></li>
     </ul>
