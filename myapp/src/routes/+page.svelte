c324e8a4e5dee025199fe013f5ce8cb1f775c242
lisavanmansom <lisavanmansom@gmail.com>
2024-09-17 12:00:41 +0200

    #11 html opzet

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import BinderElement from '$lib/binderElement.svelte';
+
+</script>
+
+<main>
+    <h1>Welcome to my</h1>
+
+    <BinderElement />
+
+    <h2>Lisa van Mansom</h2>
+    <p>frontend designer</p>
+
+</main>
+
+<footer>
+    <ul>
+        <li><a href="https://github.com/lisavanmansom">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/lisa-van-mansom-b78059231/">Linkedin</a></li>
+        <li><a href="mailto: lisavanmansom@gmail.com" >lisavanmansom@gmail.com</a></li>
+        <li><a href="tel:0642629104">0642629104</a></li>
+        <li><a href="https://github.com/lisavanmansom">Learning journal</a></li>
+    </ul>
+
+</footer>
+
+<style></style>
