5f9df96d6e6a9564959005ed6f852b402f2c617a
lisavanmansom <lisavanmansom@gmail.com>
2024-10-05 20:08:31 +0200

    update

--- myapp/src/routes/about/+page.svelte
+++ myapp/src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import Footer from '$lib/c-footer.svelte';
 </script>
 
 <main>
@@ -11,157 +11,340 @@
     <section>
         <h2>Selected works</h2>
 
+        <div class="l-d">
         <article>
+            <a href="https://github.com/lisavanmansom/proof-of-concept" class="a-d">
             <h3>Fresk digital</h3>
-            <p>For Fresk digital I designed and builded a data overviewpage. On this page it shows data from Google analytics. This project includes a loginpage, preface-page and a dashboard-page.</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Express, EJS & Node</li>
-                <li>CSS & JS</li>
-                <li>Chart.js</li>
-                <li>Dotenv</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <p>For Fresk digital I designed and builded a data overviewpage. On this page it shows data from Google analytics. This project includes a loginpage, preface-page and a dashboard-page.</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Express, EJS & Node</li>
+                        <li>CSS & JS</li>
+                        <li>Chart.js</li>
+                        <li>Dotenv</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="website for fresk digital" src="../images/fresk-digital.png">
         </article>
 
         <article>
+            <a href="https://github.com/lisavanmansom/pleasurable-ui" class="a-d">
             <h3>Tumi Mundo</h3>
-            <p>Tumi Mundo is comparable to Duolingo, only Tumi Mundp is for children. For Tumi Mundo I designed and builded a lessons page, here you can navigate to stories, lessons and playlists with stories</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Express, EJS & Node</li>
-                <li>CSS & JS</li>
-                <li>GSAP</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <p>Tumi Mundo is comparable to Duolingo, only Tumi Mundo is for children. For Tumi Mundo I designed and builded a lessons page, here you can navigate to stories, lessons and playlists with stories</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Express, EJS & Node</li>
+                        <li>CSS & JS</li>
+                        <li>GSAP</li>
+                    </ul>
+                    </div>
+                </a>
+            <img width="100" height="100" alt="website for Tumi Mundo" src="../images/tumi-Mundo.png">
         </article>
 
         <article>
+            <a href="https://github.com/lisavanmansom/fix-the-flow-interactive-website" class="a-d">
             <h3>Funda</h3>
-            <p>For Funda I designed and builded a saved houses page, here users can view their liked houses and share it with others.</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <p>For Funda I designed and builded a saved houses page, here users can view their liked houses and share it with others.</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="website for funda" src="../images/funda.png">
         </article>
 
         <article>
-            <h3>BNR</h3>
-            <p>For BNR I designed and builded an page where you can listen live to their radio, I have also made a page with info about their app.</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-            </ul>
+            <a href="https://github.com/lisavanmansom/the-startup-website-bnr" class="a-d">
+                <h3>BNR</h3>
+                <h4>frontend design</h4>
+                <div>
+                    <p>For BNR I designed and builded an page where you can listen live to their radio, I have also made a page with info about their app.</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="website for bnr" src="../images/bnr.png">
         </article>
 
         <article>
-            <h3>Profile card / 2</h3>
-            <p>As a profile card I decided to take inspiration from grotesque, maximallism styles. On the profile card is basic information about me</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Express, EJS & Node</li>
-                <li>CSS & JS</li>
-            </ul>
+            <a href="https://github.com/lisavanmansom/connect-your-tribe-profile-card" class="a-d">
+                <h3>Profile card / 2</h3>
+                <h4>frontend design</h4>
+                <div>
+                    <p>As a profile card I decided to take inspiration from grotesque, maximallism styles. On the profile card is basic information about me</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Express, EJS & Node</li>
+                        <li>CSS & JS</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="" src="../images/profilecard-2.png">
         </article>
 
         <article>
+            <a href="https://github.com/lisavanmansom/connect-your-tribe-squad-page" class="a-d">
             <h3>Squadpage / 2</h3>
-            <p>museam squadpage</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Express, EJS & Node</li>
-                <li>CSS & JS</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <p>museam squadpage</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Express, EJS & Node</li>
+                        <li>CSS & JS</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="website for fresk digital" src="../images/fresk-digital.png">
         </article>
 
         <article>
+            <a href="https://github.com/lisavanmansom/tribeforlife-squadpage" class="a-d">
             <h3>Squadpage / 1</h3>
-            <p>On this squadpage you can find all the members that are part of the squad and get in contact with them. This was a group task in which 5 people took part, for this reason there weren't really as much tasks to be done for everyone.</p>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Express, EJS & Node</li>
-                <li>CSS & JS</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <p>On this squadpage you can find all the members that are part of the squad and get in contact with them. This was a group task in which 5 people took part, for this reason there weren't really as much tasks to be done for everyone.</p>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Express, EJS & Node</li>
+                        <li>CSS & JS</li>
+                    </ul>
+                </div>
+            </a>
+            <img width="100" height="100" alt="#" src="../images/squadpage-2.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Intership Tigioo</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Adobe ID</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Photoshop</li>
-                <li>Adobe DN</li>
-            </ul>
+            <h4>graphic design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Adobe ID</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe DN</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="#" src="../images/tigioo.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Intership Positive things</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Adobe ID</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Photoshop</li>
-                <li>Adobe DN</li>
-            </ul>
+            <h4>graphic design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Adobe ID</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe DN</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="" src="../images/pt.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Graduate project</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Adobe ID</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Photoshop</li>
-                <li>Adobe DN</li>
-            </ul>
+            <h4>graphic design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Adobe ID</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe DN</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="" src="../images/ukiyo-ki.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Concept Redesign</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Adobe ID</li>
-                <li>Adobe Illustrator</li>
-                <li>Adobe Photoshop</li>
-                <li>Adobe DN</li>
-            </ul>
+            <h4>graphic design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Adobe ID</li>
+                        <li>Adobe Illustrator</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe DN</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="" src="../images/concept-1.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Concept Squadpage</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Figma</li>
-            </ul>
+            <h4>frontend design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Figma</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="" src="../images/concept-squadpage.png">
         </article>
 
         <article>
+            <div class="a-d">
             <h3>Other projects</h3>
-            <p><bold>Tools</bold></p>
-            <ul>
-                <li>Adobe Photoshop</li>
-                <li>Adobe DN</li>
-            </ul>
+            <h4>graphic design</h4>
+                <div>
+                    <ul>
+                        <li><bold>Tools</bold></li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe DN</li>
+                    </ul>
+                </div>
+            </div>
+            <img width="100" height="100" alt="" src="../images/other.png">
         </article>
 
         <article>
             <h3>Drawings / 1</h3>
+            <img width="100" height="100" alt="" src="../images/drawings-1.png">
         </article>
 
         <article>
             <h3>Drawings / 2</h3>
+            <img width="100" height="100" alt="" src="../images/drawings-2.png">
         </article>
+    </div>
+    </section>
+
+    <section>
+        <h2>about me</h2>
     </section>
 
     
 </main>
 
-
+<Footer />
 
 <style>
+main {
+        background-color: var(--beijo); color: var(--jupiter);
+        display: flex; flex-direction: column;
+        align-items: center; justify-content: center;
+        overflow-x: hidden;
+        width: 100%; min-height: 100dvh;
+        text-transform: uppercase;
+    }
     
+    h1 { font-weight: 200; font-size: 2.5em; padding: .5em;}
+    h2 { font-weight: 200; font-size: 2.2em; text-align: start;}
+    h3 { font-weight: 400; font-size: 1.9em; padding: 0 0 .25em .25em; }
+    h4 { font-weight: 200; font-size: 1.1em; order: -1; padding-left: .5em; }
+    a  { text-decoration: none; cursor: pointer; color: var(--beijo);}
+
+    header p { 
+        font-size: 2.5em; 
+        font-weight: 200; 
+        padding: 1.5em .5em .5em .5em;
+    }
+
+    section {
+        align-items: start; justify-content: space-evenly;
+        display: flex; flex-direction: column;
+        margin: 4em 0 0 3.5em;
+        width: 100%; min-height: 55em;
+    }
+
+    .l-d { 
+        display: flex;
+        flex-wrap: nowrap;
+        gap: 1.5em;
+        overflow: scroll;
+        width: 90%;
+        scroll-snap-type: x mandatory;}
+
+    article { 
+        align-items: center;
+        background-color: var(--areia);
+        border: .1em solid var(--beijo);
+        border-radius: 10px;
+        color: var(--beijo);
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        height: 45em;
+        line-height: 1.8em;
+        padding: 1.25em .5em .5em .5em;
+        min-width: 18em;
+        scroll-snap-align: center;
+    }
+
+    article img {
+        border: .1em solid var(--beijo);
+        border-radius: 10px;
+        min-height: 12em;
+        object-fit: cover;
+        order: -1;
+        min-width: 16em;
+    }
+
+    .a-d {
+        border: .1em solid var(--beijo);        
+        border-radius: 10px;
+        display: flex;
+        flex-direction: column;
+        height: 29.25em;
+        padding: .5em 0;
+        width: 16em;
+    }
+
+    .a-d div {
+        border-top: .1em solid var(--beijo);
+        display: flex;
+        flex-direction: column;
+        gap: .5em;
+        line-height: 1.5em;
+        padding: .5em;
+    }
+
+    article p {
+        font-size: 1em;        
+    }
+
+    li {
+        font-weight: 100;
+        margin-left: 1.5em ;
+    }
+
+    li:first-of-type { 
+        font-weight: 400; 
+        margin: 0;
+        list-style: none;
+    }
+
+    article:nth-last-of-type(2n) {background-color: var(--kalimba);}
+	article:nth-last-of-type(3n) {background-color: var(--jupiter);}
+    article:nth-last-of-type(4n) {background-color: var(--solitario);}
 
 </style>
